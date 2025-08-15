2fd5a4961b51e104c6adbf9d9dba3d4f1f55581b
James Ward <james.ward@arm.com>
2025-08-15 17:13:47 +0100

    MLXPK-1738 Fix notebooks tqdm bar being truncated in browsers
    Signed-off-by: James Ward <james.ward@arm.com>
    Change-Id: Ieb6ca9ced1c4895b68235b8719d3c6861c509f5f

--- src/ng_model_gym/evaluator.py
+++ src/ng_model_gym/evaluator.py
@@ -10,7 +10,7 @@ from typing import Optional
 
 import torch
 import torchvision
-from tqdm import tqdm
+from tqdm.autonotebook import tqdm
 
 from ng_model_gym.dataloader import get_dataloader
 from ng_model_gym.nss.dataloader.utils import DataLoaderMode
