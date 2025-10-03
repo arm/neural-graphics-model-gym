b35d9036fd2a1d107cba3c867f78a8d3f2a6e374
Sarah Blades <sarah.blades@arm.com>
2025-10-03 16:36:25 +0100

    Updated import_usecase_files() to avoid path issues (#46)
    * fixes import path issues seen in the Docker image
    
    
    Change-Id: I461e7131c5dff73703bb952892597d18e0eb90a9
    
    Signed-off-by: Sarah Blades <sarah.blades@arm.com>

--- src/ng_model_gym/usecases/__init__.py
+++ src/ng_model_gym/usecases/__init__.py
@@ -2,22 +2,16 @@
 # its affiliates <open-source-office@arm.com></text>
 # SPDX-License-Identifier: Apache-2.0
 import importlib
-import os
-from pathlib import Path
+import logging
+import pkgutil
+
+logger = logging.getLogger(__name__)
 
 
 def import_usecase_files() -> None:
-    """Function to import usecase models. This can be called to ensure models are imported."""
-    import_path = Path(__name__)
-    usecases_dir = f'{__file__.replace("__init__.py", "")}'
-    for subdir, _, files in os.walk(usecases_dir):
-        # Convert file path (path/to/module) to module import path (path.to.module)
-        import_path = subdir.replace("/", ".").split("neural-graphics-model-gym.src.")[
-            -1
-        ]
+    """Function to import usecase models and datasets,
+    importing all .py modules in the package."""
 
-        for file in files:
-            if file.endswith(".py") and not file.startswith("__"):
-                module_name = file[:-3]  # Remove the .py extension
-                importlib.import_module(f"{import_path}.{module_name}")
-                print(f"Imported module: {import_path}.{module_name}")
+    for _, name, _ in pkgutil.walk_packages(__path__, prefix=__name__ + "."):
+        importlib.import_module(name)
+        logger.info(f"Imported module: {name}")
