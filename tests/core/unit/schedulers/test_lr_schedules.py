f72c2df61b4f567c7a5a43d82ae0947fe9a2db32
James Ward <james.ward@arm.com>
2025-10-15 12:42:38 +0100

    MLXPK-1895 Fix exponential scheduler
    - Rename to ExponentialStep to differentiate from PyTorch ExponentialLR (which decays every epoch)
    - Rename and clarify decay_step to decay_factor param to better reflect its behaviour
    
    Signed-off-by: James Ward <james.ward@arm.com>
    Change-Id: I5ba5bea40c959909ec9150fe1e1ec07dff16b3db

--- tests/core/unit/schedulers/test_lr_schedules.py
+++ tests/core/unit/schedulers/test_lr_schedules.py
@@ -2,9 +2,7 @@
 # its affiliates <open-source-office@arm.com></text>
 # SPDX-License-Identifier: Apache-2.0
 import unittest
-from unittest import expectedFailure
 
-import numpy as np
 import torch
 from pydantic import ValidationError
 
@@ -97,145 +95,41 @@ class TestLRScheduleFunctions(unittest.TestCase):
                     msg=f"Learning rate different at index {i}",
                 )
 
-    @expectedFailure
-    def test_exponential_schedule(self):
-        """Test exponential learning rate schedule matches tf.keras."""
+    def test_exponential_step_schedule(self):
+        """Test exponential step learning rate schedule decreases every step_size."""
 
         test_weights = [torch.ones([10])]
         lr = 0.01
-        train_size = 20000
-        epochs = 15
+        train_size = 5
+        epochs = 10
 
         params_dict = self.params.model_dump()
         params_dict["train"]["fp32"]["lr_scheduler"] = {}  # Remove default lr_scheduler
 
         params_dict["train"]["fp32"]["number_of_epochs"] = epochs
-        params_dict["train"]["fp32"]["lr_scheduler"]["type"] = "exponential"
+        params_dict["train"]["fp32"]["lr_scheduler"]["type"] = "exponential_step"
         params_dict["train"]["fp32"]["lr_scheduler"]["decay_rate"] = 0.977
-        params_dict["train"]["fp32"]["lr_scheduler"]["decay_step"] = 1
-
+        params_dict["train"]["fp32"]["lr_scheduler"]["decay_factor"] = 5
         self.params = validate_params(params_dict)
 
-        optimizer = torch.optim.Adam(test_weights, lr=lr)
+        decay_step_size = (epochs * train_size) // 5
 
+        optimizer = torch.optim.Adam(test_weights, lr=lr)
         lr_sched = get_lr_schedule(
             self.params.train.fp32, optimizer, train_size, self.params
         )
+
         self.assertIsInstance(lr_sched, torch.optim.lr_scheduler.StepLR)
         self.assertEqual(lr_sched.gamma, self.params.train.fp32.lr_scheduler.decay_rate)
+        self.assertEqual(lr_sched.step_size, decay_step_size)
 
-        torch_lr_produced = []
-        for _ in range(epochs):
-            for _ in range(train_size):
-                if lr_sched.get_last_lr()[0] not in torch_lr_produced:
-                    torch_lr_produced.append(lr_sched.get_last_lr()[0])
-                lr_sched.step()
-
-        tf_keras_lr_schedule_values = [
-            0.009999999776482582,
-            0.009769999422132969,
-            0.009545289911329746,
-            0.009325748309493065,
-            0.009111255407333374,
-            0.00890169758349657,
-            0.008696957491338253,
-            0.00849692802876234,
-            0.008301498368382454,
-            0.008110564202070236,
-            0.00792402122169733,
-            0.007741768844425678,
-            0.007563707884401083,
-            0.007389742415398359,
-            0.007219778839498758,
-            0.007053723558783531,
-            0.006891487631946802,
-            0.006732983514666557,
-            0.006578125059604645,
-            0.006426828447729349,
-            0.006279011256992817,
-            0.0061345938593149185,
-            0.005993498023599386,
-            0.005855647847056389,
-            0.005720967426896095,
-            0.005589385516941547,
-            0.005460829474031925,
-            0.0053352308459579945,
-            0.005212520249187946,
-            0.00509263202548027,
-            0.004975501913577318,
-            0.004861065186560154,
-            0.00474926084280014,
-            0.004640027415007353,
-            0.004533306695520878,
-            0.004429040942341089,
-            0.004327172879129648,
-            0.00422764802351594,
-            0.004130411893129349,
-            0.004035412799566984,
-            0.00394259812310338,
-            0.0038519182708114386,
-            0.003763324348255992,
-            0.0036767679266631603,
-            0.0035922019742429256,
-            0.003509581321850419,
-            0.0034288610331714153,
-            0.0033499973360449076,
-            0.0032729473896324635,
-            0.003197669517248869,
-            0.0031241232063621283,
-            0.0030522681772708893,
-            0.002982066012918949,
-            0.0029134785290807486,
-            0.0028464687056839466,
-            0.002780999755486846,
-            0.0027170367538928986,
-            0.0026545450091362,
-            0.0025934905279427767,
-            0.00253384024836123,
-            0.0024755618069320917,
-            0.002418623771518469,
-            0.002362995408475399,
-            0.002308646449819207,
-            0.0022555477917194366,
-            0.002203670097514987,
-            0.0021529856603592634,
-            0.0021034670062363148,
-            0.002055087126791477,
-            0.002007820177823305,
-            0.001961640315130353,
-            0.0019165226258337498,
-            0.0018724426627159119,
-            0.001829376444220543,
-            0.0017873008036985993,
-            0.0017461928073316813,
-            0.0017060304526239634,
-            0.0016667917370796204,
-            0.0016284554731100798,
-            0.0015910009387880564,
-            0.0015544079942628741,
-            0.0015186566160991788,
-            0.001483727479353547,
-            0.001449601724743843,
-            0.0014162608422338963,
-            0.0013836869038641453,
-            0.0013518620980903506,
-            0.0013207693118602037,
-            0.0012903916649520397,
-            0.0012607125099748373,
-            0.0012317161308601499,
-            0.001203386695124209,
-            0.0011757088359445333,
-            0.0011486675357446074,
-            0.001122248126193881,
-            0.0010964364046230912,
-            0.0010712184011936188,
-            0.0010465803788974881,
-            0.001022509066388011,
-            0.000998991308733821,
-        ]
-
-        self.assertEqual(len(torch_lr_produced), len(tf_keras_lr_schedule_values))
-        self.assertTrue(np.allclose(torch_lr_produced, tf_keras_lr_schedule_values))
+        old_lr = lr_sched.get_last_lr()[0]
+        for _ in range(train_size * epochs):
+            lr_sched.step()
+            if (lr_sched.last_epoch % decay_step_size) == 0:
+                new_lr = lr_sched.get_last_lr()[0]
+                self.assertAlmostEqual(new_lr, old_lr * lr_sched.gamma, places=7)
+                old_lr = new_lr
 
     def test_static_schedule(self):
         """Test static learning rate schedule returns None."""
@@ -293,13 +187,13 @@ class TestSchedulerConfig(unittest.TestCase):
     def test_exponential_valid(self):
         params = self.params
         params["train"]["qat"]["lr_scheduler"] = {
-            "type": "exponential",
+            "type": "exponential_step",
             "decay_rate": 0.9,
-            "decay_step": 5,
+            "decay_factor": 5,
         }
         cfg = ConfigModel(**params)
-        self.assertEqual(cfg.train.qat.lr_scheduler.type, "exponential")
-        self.assertEqual(cfg.train.qat.lr_scheduler.decay_step, 5)
+        self.assertEqual(cfg.train.qat.lr_scheduler.type, "exponential_step")
+        self.assertEqual(cfg.train.qat.lr_scheduler.decay_factor, 5)
 
     def test_static_valid(self):
         params = self.params
@@ -325,22 +219,22 @@ class TestSchedulerConfig(unittest.TestCase):
         with self.assertRaises(ValidationError):
             ConfigModel(**params)
 
-    def test_exponential_missing_required_field(self):
+    def test_exponential_step_missing_required_field(self):
         params = self.params
         params["train"]["fp32"]["lr_scheduler"] = {
-            "type": "exponential",
+            "type": "exponential_step",
             "decay_rate": 0.95,
-            # decay_step missing
+            # decay_factor missing
         }
         with self.assertRaises(ValidationError):
             ConfigModel(**params)
 
-    def test_wrong_field_for_exponential(self):
+    def test_wrong_field_for_exponential_step(self):
         params = self.params
         params["train"]["fp32"]["lr_scheduler"] = {
-            "type": "exponential",
-            "warmup_percentage": 0.1,  # illegal for exponential
-            "decay_rate": 0.9,
+            "type": "exponential_step",
+            "warmup_percentage": 0.1,  # illegal for exponential_step
+            "decay_factor": 0.9,
             "decay_step": 10,
         }
         with self.assertRaises(ValidationError):
