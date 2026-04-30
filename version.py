c4b3c06a7651d2aebe5bce4ea936fe001a7607fe
Ayaan Masood <ayaan.masood@arm.com>
2026-04-30 15:50:02 +0100

    Generate Readme for PYPI
    Signed-off-by: Ayaan Masood <ayaan.masood@arm.com>
    Change-Id: Iefd6b774492f974bc34cfce7ffdbc10880a372b6

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: <text>Copyright 2025 Arm Limited and/or
+# SPDX-FileCopyrightText: <text>Copyright 2025-2026 Arm Limited and/or
 # its affiliates <open-source-office@arm.com></text>
 # SPDX-License-Identifier: Apache-2.0
 
@@ -8,13 +8,18 @@ import re
 def normalise_tag(tag: str, params: dict) -> str:  # pylint: disable=unused-argument
     """
     Normalise the tag to be PEP-440 compliant for building the wheel
-    Works with both 'upstream/v' and 'v' tags, plus optional end suffix e.g. '-rc1'
+    Works with both 'upstream/v' and 'v' tags, plus optional end suffix,
+    e.g. 'rc1' or '-rc1'
         v0.1.0          -> 0.1.0
         upstream/v0.1.0 -> 0.1.0
+        v0.1.0rc1       -> 0.1.0rc1
+        upstream/v0.1.0rc1 -> 0.1.0rc1
         v0.1.0-rc1      -> 0.1.0rc1
+        upstream/v0.1.0-rc1 -> 0.1.0rc1
     """
     tag_matched = re.match(
-        r"^(?:upstream/)?v?(?P<base>[0-9]+(?:\.[0-9]+)*)(?:-(?P<suffix>[A-Za-z0-9\.]+))?$",
+        r"^(?:upstream/)?v(?P<base>[0-9]+(?:\.[0-9]+)*)"
+        r"(?:-?(?P<suffix>[A-Za-z][A-Za-z0-9\.]*))?$",
         tag,
     )
 
